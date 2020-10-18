6da6ba59a07047e7203bd38962998214caa63b53
Ranffi Ramirez <ranffiramirez@gmail.com>
2020-10-17 20:05:11 -0400

    added Author, Book, Genre, and User models, (also fixed db.js)

---
+++ server/db/models/Book.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Book = db.define('book', {
+    title : {
+        type : Sequelize.STRING,
+        allowNull : false
+    },
+    price : {
+        type : Sequelize.INTEGER,
+    },
+    description : {
+        type : Sequelize.TEXT
+    },
+    coverImageUrl : {
+        type : Sequelize.STRING
+    },
+    quantityInStock : {
+        type : Sequelize.INTEGER
+    },
+    rating : {
+        type : Sequelize.INTEGER,
+        validate : {
+            min : 0,
+            max : 5
+        },
+        defaultValue : 5
+    },
+    featured : {
+        type : Sequelize.BOOLEAN,
+        defaultValue : false
+    },
+
+    onSale : {
+        type : Sequelize.BOOLEAN,
+        defaultValue : false
+    }
+})
+
+module.exports = Book
