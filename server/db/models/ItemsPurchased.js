bf617cf238887fd643de9fb352a1822846a5c9a4
Ranffi Ramirez <ranffiramirez@gmail.com>
2020-10-18 21:19:23 -0400

    updated models, added CartItems, ItemsPurchased, OrderHistory

---
+++ server/db/models/ItemsPurchased.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize") 
+const db = require("../db")
+
+const itemsPurchased = db.define('itemsPurchased' , {
+    quantity : {
+        type : Sequelize.INTEGER,
+    }
+})
+
+module.exports = itemsPurchased
---
+++ server/db/models/OrderHistory.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize") 
+const db = require("../db")
+
+const OrderHistory = db.define('orderHistory' , {
+    shippingAddress : {
+        type : Sequelize.TEXT
+    },
+
+    subTotal : {
+        type : Sequelize.FLOAT
+    },
+    quantity : {
+        type : Sequelize.INTEGER
+    },
+    status : {
+        type : Sequelize.STRING
+    }
+})
+
+module.exports = OrderHistory
