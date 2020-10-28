d6401140c28334766a8b579f0a7848dff7c16501
antonpreku <antonpreku@hotmail.com>
2020-10-27 22:01:20 -0400

    cart_Update_2

--- server/db/models/CartItem.js
+++ server/db/models/CartItem.js
@@ -1,19 +1,11 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-<<<<<<< HEAD
 const CartItem = db.define('cartItem' , {
     quantity : {
         type : Sequelize.INTEGER,
         allowNull : false,
         defaultValue : 1
-=======
-const CartItem = db.define('cartItem', {
-    quantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 0
->>>>>>> 89debfce6f9ad62ae298cdb0c62e1111973cbe3e
     }
 })
 
