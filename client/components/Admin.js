92630e5621b93669fbab66d58892532873fcb0d7
John Cook <jpcook72@gmail.com>
2020-10-30 08:34:02 -0500

    Seed admin; admin component skeleton

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -5,6 +5,14 @@ class Admin extends Component{
   constructor() {
     super()
     this.state = {
+      title: '',
+      price: '',
+      description: '',
+      coverImageURL: '',
+      quantityInStock: 1,
+      rating: 0,
+      featured: false,
+      onSale: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -23,7 +31,16 @@ class Admin extends Component{
   }
 
   render(){
-    return (<div><p>{`Testing admin page and user on state. State.user is user #${this.props.user.id}, with userName of ${this.props.user.userName} (guest username is null)`}</p></div>)
+    const {user} = this.props;
+    const {handleChange, handleSubmit} = this;
+
+    return !user.isAdmin ?
+    (<div><p>Check out another page!</p></div>) :
+    (
+      <div>
+        otherwise...
+      </div>
+    )
   }
 
 }
