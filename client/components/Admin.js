cd2805f3732ea9ab377561adaea9d3cdf3ba59f5
John Cook <jpcook72@gmail.com>
2020-11-01 09:00:38 -0600

    Add book-- basic functionality on /admin

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {connect} from 'react-redux';
 import React, {Component} from 'react';
 
@@ -6,11 +7,13 @@ class Admin extends Component{
     super()
     this.state = {
       title: '',
+      authorFirstName: '',
+      authorLastName: '',
       price: '',
       description: '',
       coverImageURL: '',
       quantityInStock: 1,
-      rating: 0,
+      rating: 3,
       featured: false,
       onSale: false
     }
@@ -24,21 +27,77 @@ class Admin extends Component{
     })
   }
 
-  handleSubmit(ev){
+  async handleSubmit(ev){
     ev.preventDefault()
+    await axios.post(`/api/books`, {...this.state})
+
     this.setState({
+      title: '',
+      authorFirstName: '',
+      authorLastName: '',
+      price: '',
+      description: '',
+      coverImageURL: '',
+      quantityInStock: 1,
+      rating: 3,
+      featured: false,
+      onSale: false
     })
   }
 
   render(){
     const {user} = this.props;
     const {handleChange, handleSubmit} = this;
+    const { title, authorFirstName, authorLastName, price, description, coverImageURL, quantityInStock, rating, featured, onSale } = this.state
 
-    return !user.isAdmin ?
+    return user.isAdmin ?
     (<div><p>Check out another page!</p></div>) :
     (
       <div>
-        otherwise...
+        <h2>
+          Add Book
+        </h2>
+        <form onSubmit = {handleSubmit} id = "signUpForm">
+          <div id = "signUpUserInfo">
+            <label htmlFor = "title" className = "signUpLabel">Title:</label>
+            <input name = "title" className = "signUpInput" onChange = {handleChange} value = {title} />
+
+            <label htmlFor = "authorFirstName" className = "signUpLabel">Author First Name:</label>
+            <input name = "authorFirstName" className = "signUpInput" onChange = {handleChange} value = {authorFirstName} />
+
+            <label htmlFor = "authorLastName" className = "signUpLabel">Author Last Name:</label>
+            <input name = "authorLastName" className = "signUpInput" onChange = {handleChange} value = {authorLastName} />
+
+            <div className="input-icon">
+              <label htmlFor = "price" className = "signUpLabel">Price:</label>
+              <input name = "price" className = "signUpInput" onChange = {handleChange} value = {price} />
+              <i>$</i>
+            </div>
+
+            <label htmlFor = "coverImageURL" className = "signUpLabel">Image URL:</label>
+            <input name = "coverImageURL" className = "signUpInput" onChange = {handleChange} value = {coverImageURL} />
+
+            <div className="adminCloseEnded adminNums">
+              <label htmlFor = "quantityInStock" className = "signUpLabel">In stock:</label>
+              <input name = "quantityInStock" id="inStock" type="number" min="0" step="1" className = "signUpInput" onChange = {handleChange} value = {quantityInStock} />
+
+              <label htmlFor = "title" className = "rating">Rating (1-5):</label>
+              <input name = "rating" type="number" min="1" max="5" step="1" className = "signUpInput" onChange = {handleChange} value = {rating} />
+            </div>
+            <div className="adminCloseEnded adminChecks">
+              <input name = "featured" type="checkbox" className = "signUpInput" onChange = {handleChange} value = {featured} />
+              <label htmlFor = "featured" className = "signUpLabel">Featured</label>
+
+              <input name = "onSale" type="checkbox" className = "signUpInput" onChange = {handleChange} value = {onSale} />
+              <label htmlFor = "onSale" className = "signUpLabel">On Sale</label>
+
+            </div>
+
+            <label htmlFor = "description" className = "signUpLabel">Description:</label>
+            <textarea name = "description" className = "signUpInput" onChange = {handleChange} value = {description} />
+          </div>
+        <button type = "submit" id = "signUpSubmit">Add Book</button>
+        </form>
       </div>
     )
   }
