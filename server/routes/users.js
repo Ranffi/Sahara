5e80298e0cd18d3cb438745a7d06ba548099733c
John Cook <jpcook72@gmail.com>
2020-11-04 19:06:27 -0600

    Gating user api routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,14 +3,6 @@ const bcrypt = require('bcrypt')
 const { User, Cart } = require('../db');
 const Session = require('../db/models/Session');
 
-router.get('/whoami', (req, res) => {
-    if (req.user) {
-        res.send(`Welcome back ${req.user.userName}`)
-    } else {
-        res.send('you are not logged in')
-    }
-})
-
 router.get('/get-user', (req, res, next) => {
     try {
         res.send(req.user)
@@ -22,25 +14,30 @@ router.get('/get-user', (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
     try {
-        const {userName, password, firstName, lastName,  email, shippingAddressId, isGuest } = req.body
-        const hashedPw = await bcrypt.hash(password, 10)
-        const user = await User.findByPk(req.params.id);
-        await user.update({
-            userName,
-            password: hashedPw,
-            email,
-            firstName,
-            lastName,
-            shippingAddressId,
-            isGuest
-        })
-        await Session.update({
-            userId: user.id
-        },
-        {
-            where: {id: req.cookies.sid}
-        })
-        res.send(user)
+        if (req.user.id === Number(req.params.id) || req.user.isAdmin) {
+            const {userName, password, firstName, lastName,  email, shippingAddressId, isGuest } = req.body
+            const hashedPw = await bcrypt.hash(password, 10)
+            const user = await User.findByPk(req.params.id);
+            await user.update({
+                userName,
+                password: hashedPw,
+                email,
+                firstName,
+                lastName,
+                shippingAddressId,
+                isGuest
+            })
+            await Session.update({
+                userId: user.id
+            },
+            {
+                where: {id: req.cookies.sid}
+            })
+            res.send(user)
+        }
+        else {
+            console.log('nice yup try', req.params.id, req.user.id)
+        }
     }
     catch (err) {
         next(err)
@@ -49,7 +46,15 @@ router.put('/:id', async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
     try {
-        res.send(await User.findAll( {where:{isGuest: false}}));
+        if (req.user.isAdmin) {
+            const allUsers = await User.findAll( {where: { isGuest: false }});
+            res.send(allUsers.map(user => {
+                return {id: user.id, userName: user.userName}
+            }))
+        }
+        else {
+            res.send('nice try')
+        }
     }
     catch (err) {
         next(err)
@@ -58,7 +63,15 @@ router.get('/', async (req, res, next) => {
 
 router.get('/admins', async (req, res, next) => {
     try {
-        res.send(await User.findAll( {where: { adminStatus: true }}));
+        if (req.user.isAdmin) {
+            const allUsers = await User.findAll( {where: { adminStatus: true }});
+            res.send(allUsers.map(user => {
+                return {id: user.id, userName: user.userName}
+            }))
+        }
+        else {
+            res.send('nice try')
+        }
     }
     catch (err) {
         next(err)
@@ -67,9 +80,11 @@ router.get('/admins', async (req, res, next) => {
 
 router.delete('/:userId', async (req, res, next) => {
     try {
-        const user = await User.findByPk(req.params.userId)
-        await user.destroy();
-        res.sendStatus(204)
+        if (req.user.id === Number(req.params.userId) || req.user.isAdmin) {
+            const user = await User.findByPk(req.params.userId)
+            await user.destroy();
+            res.sendStatus(204)
+        }
     }
     catch (err) {
         next(err)
@@ -78,52 +93,16 @@ router.delete('/:userId', async (req, res, next) => {
 
 router.put('/admin/:userId', async (req, res, next) => {
     try {
-        await User.update({adminStatus: req.body.adminStatus},
-            {where: {id: req.params.userId}})
-        res.sendStatus(204)
+        if (req.user.isAdmin) {
+            await User.update({adminStatus: req.body.adminStatus},
+                {where: {id: req.params.userId}})
+            res.sendStatus(204)
+        }
     }
     catch (err) {
         next(err)
     }
 })
 
-
-router.post('/:userId/books/:bookId', async (req, res, next) => {
-    try {
-        res.send(await Cart.create({
-            ...req.body,
-            userId: req.params.userId,
-            bookId: req.params.bookId
-        }));
-    }
-    catch (err) {
-        next(err)
-    }
-})
-
-router.put('/:userId/books/:bookId', async (req, res, next) => {
-    try {
-        //this is written as if one row per product in cart
-        //may need to specify put route to specific row in cart db
-        let order = await Cart.findAll({
-            where: {
-                bookId: req.params.bookId,
-                userId: req.params.userId
-            }
-        })
-        await order[0].update(req.body);
-        let cartOrder = await Cart.findAll({
-            where: {
-                bookId: req.params.bookId,
-                userId: req.params.userId
-            },
-            include: { all: true, nested: true }
-        })
-        res.send(cartOrder[0]);
-    }
-    catch (err) {
-        next(err)
-    }
-})
 
 module.exports = router
