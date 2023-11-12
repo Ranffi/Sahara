c46f43ef5be23e6e5a8fa1ab89f626bdee3114d2
Ranffi Ramirez <ranffiramirez@gmail.com>
2023-11-12 11:06:46 -0500

    more small edits

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,47 +1,43 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return (
+  return (
     <footer>
-  <div className="footer-inner" >
-    <section className="footer-item">
-      <h1 className = "footer_h1">Sahara</h1>
+      <div className="footer-inner" >
+        <section className="footer-item">
+          <h1 className="footer_h1">Sahara</h1>
 
-      <h2 className = "footer_h2">Get Lost <br />in the story.</h2>
-    </section>
+          <h2 className="footer_h2">Get Lost <br />in the story.</h2>
+        </section>
 
-    <section className="footer-item">
-      <h3 className = "footer_h3">Explore</h3>
-        <ul className = "footer_ul">
-        <Link to="/books"><li className = "footer_li"><ul className = "footer_a"href="#">All Books</ul></li></Link>
-        </ul>
-    </section>
+        <section className="footer-item">
+          <h3 className="footer_h3">Explore</h3>
+          <ul className="footer_ul">
+            <Link to="/books"><li className="footer_li"><ul className="footer_a" href="#">All Books</ul></li></Link>
+          </ul>
+        </section>
 
-    <section className="footer-item">
+        <section className="footer-item">
 
-        <h3 className = "footer_h3">Contact us</h3>
-        <p className = "footer_p"> <a className = "footer_a" href="#">Sahara@gmail.com</a></p>
-        <p className = "footer_p"><a className = "footer_a" href="#">999.333.7777</a></p>
+          <h3 className="footer_h3">Contact us</h3>
+          <p className="footer_p"> <a className="footer_a" href="#">Sahara@gmail.com</a></p>
+          <p className="footer_p"><a className="footer_a" href="#">999.333.7777</a></p>
 
-    </section>
-
-
-    <section className="footer-item">
-      <h3 className = "footer_h3"> Follow</h3>
-        <ul className = "footer_ul">
-          <li className = "footer_li"><a className = "footer_a" href="https://www.instagram.com/" target = "_blank">Instagram</a></li>
-          <li className = "footer_li"><a className = "footer_a" href="https://www.facebook.com/" target = "_blank">Twitter</a></li>
-          <li className = "footer_li"><a className = "footer_a" href="https://www.LinkedIn.com/" target = "_blank">LinkedIn</a></li>
-        </ul>
-    </section>
+        </section>
 
 
-    <section className="footer-item">
-        <Link to = "/about"><ul className = "footer_a" href="#" className="footer-button">Next: About</ul></Link>
-    </section>
-  </div>
+        <section className="footer-item">
+          <h3 className="footer_h3"> Follow</h3>
+          <ul className="footer_ul">
+            <li className="footer_li"><a className="footer_a" href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+            <li className="footer_li"><a className="footer_a" href="https://www.facebook.com/" target="_blank">Twitter</a></li>
+            <li className="footer_li"><a className="footer_a" href="https://www.LinkedIn.com/" target="_blank">LinkedIn</a></li>
+          </ul>
+        </section>
+
+      </div>
     </footer>
-    )
+  )
 }
 export default Footer
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Cart from './Cart'
 import Sanduich from './sanduich'
-import {getBooks, getAuthors, getAuthorBooks, getGenre, getGenreBooks, singleBook} from '../redux/books'
+import { getBooks, getAuthors, getAuthorBooks, getGenre, getGenreBooks, singleBook } from '../redux/books'
 
-class NavBar extends Component{
-  constructor(){
+class NavBar extends Component {
+  constructor() {
     super();
     this.state = {
       name: false,
@@ -17,41 +17,41 @@ class NavBar extends Component{
     this.searchChenge = this.searchChenge.bind(this)
     this.searchBy = this.searchBy.bind(this)
     this.emtyValue = this.emtyValue.bind(this)
-}
-componentDidMount(){
-  this.props.getAuthors()
-  this.props.getGenre()
-}
-
-searchChenge(ev){
-  this.setState({value: ev.target.value})
-}
-
-handleSubmit(ev){
-  ev.preventDefault()
-
-}
-  searchBy(ev){
-    this.setState({choice: ev.target.value})
   }
-  emtyValue(id, value){
-    this.setState({value: ''})
-    if (value === 'Author' ){
+  componentDidMount() {
+    this.props.getAuthors()
+    this.props.getGenre()
+  }
+
+  searchChenge(ev) {
+    this.setState({ value: ev.target.value })
+  }
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+
+  }
+  searchBy(ev) {
+    this.setState({ choice: ev.target.value })
+  }
+  emtyValue(id, value) {
+    this.setState({ value: '' })
+    if (value === 'Author') {
       this.props.authorBooks(id)
-    } else if (value === 'Genre' ){
+    } else if (value === 'Genre') {
       this.props.genreBooks(id)
     } else {
       this.props.getSingelbook(id)
     }
   }
-  addClass(){
+  addClass() {
     // eslint-disable-next-line react/no-access-state-in-setstate
-    this.setState({name: !this.state.name})
+    this.setState({ name: !this.state.name })
   }
-  render(){
+  render() {
     const filter = this.state.value.toLocaleUpperCase()
-    const {books, cartItems, authors, genre, user} = this.props
-    const {value, choice, name} = this.state
+    const { books, cartItems, authors, genre, user } = this.props
+    const { value, choice, name } = this.state
     if (!books) return (<div>Loading...</div>)
     return (
       <div>
@@ -59,72 +59,71 @@ handleSubmit(ev){
           <div>
             {
               !user.isGuest ?
-              <Sanduich />
-              : ''
+                <Sanduich />
+                : ''
             }
           </div>
-          <div id = "navLeftContainer">
-            <Link className = "navLink" to = "/">Home</Link>
-            <Link className = "navLink" to = "/about">About</Link>
-            <Link className = "navLink" to = "/books" onClick={() => this.props.getBook()}>Books</Link>
+          <div id="navLeftContainer">
+            <Link className="navLink" to="/">Home</Link>
+            <Link className="navLink" to="/books" onClick={() => this.props.getBook()}>Books</Link>
           </div>
-          <div id = "navCenterContainer">
-          <select className="searchDropdown" onChange={this.searchBy}  >
+          <div id="navCenterContainer">
+            <select className="searchDropdown" onChange={this.searchBy}  >
               <option value="Books">Books</option>
               <option value="Author">Author</option>
               <option value="Genre">Genre</option>
-          </select>
+            </select>
             <form onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="Search..." name="search" value={this.state.value} onChange={this.searchChenge} autoComplete="off" />
-            <button type="submit"><i className="fa fa-search" /></button>
+              <input type="text" placeholder="Search..." name="search" value={this.state.value} onChange={this.searchChenge} autoComplete="off" />
+              <button type="submit"><i className="fa fa-search" /></button>
             </form>
           </div>
-          <div id ="navRightContainer">
+          <div id="navRightContainer">
             {
               user.isGuest ?
-              <div className="loginContainer">
-                <Link className = "navLink" to = "/login">Log In</Link>
-                <Link className = "navLink" to = "/signUp">Sign Up</Link>
-              </div>
-              :
-              <div className="logoutContainer">
-                <h4>Welcome, {user.firstName} </h4>
-              </div>
+                <div className="loginContainer">
+                  <Link className="navLink" to="/login">Log In</Link>
+                  <Link className="navLink" to="/signUp">Sign Up</Link>
+                </div>
+                :
+                <div className="logoutContainer">
+                  <h4>Welcome, {user.firstName} </h4>
+                </div>
             }
           </div>
-          <div className="cart-btn" onClick={ () => this.addClass()}>
-              <span className="nav-icon" >
-                  <i className="fas fa-cart-plus" />
-              </span>
-              <div className="cart-items">{cartItems ? cartItems.length : 0}</div>
+          <div className="cart-btn" onClick={() => this.addClass()}>
+            <span className="nav-icon" >
+              <i className="fas fa-cart-plus" />
+            </span>
+            <div className="cart-items">{cartItems ? cartItems.length : 0}</div>
           </div>
         </nav>
         <Cart hideClass={name} addClass={this.addClass} />
         <ul id="myUL" className={value !== '' ? '' : 'hidden'}>
           {
             choice === 'Books' ?
-            books.map( book => {
-              if (book.title.toUpperCase().indexOf(filter) > -1 && filter !== '') {
+              books.map(book => {
+                if (book.title.toUpperCase().indexOf(filter) > -1 && filter !== '') {
                   return (
-                    <Link to={`/books/${book.id}`} key ={book.id} onClick={() => this.emtyValue(book.id)} >{book.title}</Link>
+                    <Link to={`/books/${book.id}`} key={book.id} onClick={() => this.emtyValue(book.id)} >{book.title}</Link>
                   )
                 }
               }) :
               choice === 'Author' ?
-              authors.map( author => {
-                if (author.firstName.toUpperCase().indexOf(filter) > -1 || author.lastName.toUpperCase().indexOf(filter) > -1 && filter !== '') {
+                authors.map(author => {
+                  if (author.firstName.toUpperCase().indexOf(filter) > -1 || author.lastName.toUpperCase().indexOf(filter) > -1 && filter !== '') {
                     return (
-                      <Link to ="/books" key ={author.id} onClick={() => this.emtyValue(author.id, choice)} >{author.firstName} {author.lastName}</Link>
+                      <Link to="/books" key={author.id} onClick={() => this.emtyValue(author.id, choice)} >{author.firstName} {author.lastName}</Link>
                     )
                   }
                 }) :
-                genre.map( element => {
+                genre.map(element => {
                   if (element.name.toUpperCase().indexOf(filter) > -1 && filter !== '') {
-                      return (
-                        <Link to="/books" key ={element.id} onClick={() => this.emtyValue(element.id, choice)} >{element.name}</Link>
-                      )
-                    }
-                  })
+                    return (
+                      <Link to="/books" key={element.id} onClick={() => this.emtyValue(element.id, choice)} >{element.name}</Link>
+                    )
+                  }
+                })
           }
         </ul>
       </div>
@@ -133,20 +132,23 @@ handleSubmit(ev){
 }
 
 export default connect(
-({books, items, user}) => {return {
-  books: books.books,
-  cartItems: items.cartItems,
-  authors: books.authors,
-  genre: books.genre,
-  user: user.user
-}
-},
-(dispatch) => {return {
-  getBook: () => dispatch(getBooks()),
-  getAuthors: () => dispatch(getAuthors()),
-  authorBooks: (id) => dispatch(getAuthorBooks(id)),
-  getGenre: () => dispatch(getGenre()),
-  genreBooks: (id) => dispatch(getGenreBooks(id)),
-  getSingelbook: (id) => dispatch(singleBook(id))
-}}
+  ({ books, items, user }) => {
+    return {
+      books: books.books,
+      cartItems: items.cartItems,
+      authors: books.authors,
+      genre: books.genre,
+      user: user.user
+    }
+  },
+  (dispatch) => {
+    return {
+      getBook: () => dispatch(getBooks()),
+      getAuthors: () => dispatch(getAuthors()),
+      authorBooks: (id) => dispatch(getAuthorBooks(id)),
+      getGenre: () => dispatch(getGenre()),
+      genreBooks: (id) => dispatch(getGenreBooks(id)),
+      getSingelbook: (id) => dispatch(singleBook(id))
+    }
+  }
 )(NavBar)
